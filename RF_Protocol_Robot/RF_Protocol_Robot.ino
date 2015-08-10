36b2e594353aee4b65fae1b5343179c99e85c0d5
ErwinMartens <erwin_martens@hotmail.com>
2015-08-10 15:13:52 +0200

    Added documentation, protocol functions written for: ping, arrived, stop and all acknowledge cases

--- RF_Protocol_Robot/RF_Protocol_Robot.ino
+++ RF_Protocol_Robot/RF_Protocol_Robot.ino
@@ -1,3 +1,7 @@
+/* This code starts a Radio Frequency communication at 433 mHz as a slave.
+ It listens for a command and replies according to our own protocol.
+ */
+
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,8 +12,10 @@ const int drivingTime = 250;
 const int robotId = 1;
 const int broadcastId = 9;
 
+//Debug enables or disables Serial Monitor messages
 boolean debug = true;
 
+//Protocol incomming message
 int IN_ReceiverID = 0;
 int IN_SenderID = 0;
 int IN_MessageID = 0;
@@ -18,6 +24,7 @@ int IN_Value2 = 0;
 int IN_Value3 = 0;
 int IN_Checksum = 0;
 
+//Protocol outgoing message
 int OUT_ReceiverID = 0;
 #define OUT_SenderID 1
 int OUT_MessageID = 0;
@@ -26,22 +33,20 @@ int OUT_Value2 = 0;
 int OUT_Value3 = 0;
 int OUT_Checksum = 0;
 
-#define ENL 3 //analog PWM port Left
+//Motor shield controller
+#define ENL 3     //analog PWM port Left
 #define MotorL4 4 //digital port IN1
 #define MotorL5 5 //digital port IN2
-//RIGHT motor
-#define ENR 8 //analog PWM port Right
+#define ENR 8     //analog PWM port Right
 #define MotorR6 6 //digital port IN3
 #define MotorR7 7 //digital port IN4
 
 long IN_Raw_Value;
-
 char Message[8];
-
 RCSwitch mySwitch = RCSwitch();
 
 void setup () {
-  Serial.begin(9600);
+  Serial.begin(9600);  // 
   mySwitch.enableReceive(0);
 
   pinMode (ENL, OUTPUT);
@@ -68,40 +73,52 @@ void loop() {
 
       if (IN_ReceiverID == robotId || IN_ReceiverID == broadcastId) { //Message is send to this robot
 
+        OUT_ReceiverID = IN_SenderID;
+        OUT_MessageID = 8;
+        
         switch (IN_MessageID)  { //Command type
-            
-          case 0: //Ping
-            break;
-          case 1: //Battery status
-            break;
-          case 2: //Go to Home
-            break;
-          case 3: //GoTo <location>
-            break;
-          case 4: //Move <direction> <amount>
-            handleMove();
-            break;
-          case 5: //Not used yet
-            break;
-          case 6: //Not used yet
-            break;
-          case 7: //Not used yet
-            break;
-          case 8: //Acknowledge
-            break;
-          case 9: //STOP
-            break;
-        }
 
-      } else {
+        case 0: //Ping
+          writeOUT_Values(0,IN_Checksum,0);
+          break;
+        case 1: //Battery status
+          //Read battery 12 V or 24 V according to variable "IN_Value1".
+          //Read percentage between 0 and 99;
+          //Split as two numbers;
+          //Write to these variables
+          writeOUT_Values(5,5,IN_Checksum);
+          OUT_MessageID = 1;
+          break;
+        case 2: //Go to Home
+          writeOUT_Values(2,IN_Value1,IN_Checksum);
+          break;
+        case 3: //GoTo <location>
+          writeOUT_Values(3,IN_Checksum,0);
+          break;
+        case 4: //Move <direction> <amount>
+          writeOUT_Values(4,IN_Checksum,0);
+          handleMove();
+          break;
+        }
+        
+        OUT_Checksum = calculateChecksum(OUT_ReceiverID, OUT_SenderID, OUT_MessageID, OUT_Value1, OUT_Value2, OUT_Value3);
+      } 
+      
+      else {
         logMessage("Message is not for this robot");
       }
-    } else {
+      
+    } 
+    
+    else {
       logMessage("Message is not valid");
       resetMessage();
     }
+    
     mySwitch.resetAvailable();
+  
   }
+  
 }
 
 /**
@@ -123,25 +140,25 @@ void handleMove() {
 
   switch (IN_Value1)  {
 
-    case 0: //drive forward
-      logMessage("Value1: Forward");
-      driveForTime(HIGH, LOW, HIGH, LOW);
-      break;
+  case 0: //drive forward
+    logMessage("Value1: Forward");
+    driveForTime(HIGH, LOW, HIGH, LOW);
+    break;
 
-    case 1: //drive reverse
-      logMessage("Value1: Reverse");
-      driveForTime(LOW, HIGH, LOW, HIGH);
-      break;
+  case 1: //drive reverse
+    logMessage("Value1: Reverse");
+    driveForTime(LOW, HIGH, LOW, HIGH);
+    break;
 
-    case 2: //drive right
-      logMessage("Value1: Right");
-      driveForTime(LOW, LOW, LOW, HIGH, turningTime, turningTime);
-      break;
+  case 2: //drive right
+    logMessage("Value1: Right");
+    driveForTime(LOW, LOW, LOW, HIGH, turningTime, turningTime);
+    break;
 
-    case 3: //drive left
-      logMessage("Value1: Left");
-      driveForTime(LOW, HIGH, LOW, LOW, turningTime, turningTime);
-      break;
+  case 3: //drive left
+    logMessage("Value1: Left");
+    driveForTime(LOW, HIGH, LOW, LOW, turningTime, turningTime);
+    break;
   }
 }
 
@@ -162,7 +179,7 @@ void sendMessage() {
 }
 
 /**
- * Sets all values with the new received message
+ * Read the incomming message and split it to seperate variables0
  */
 void readMessage() {
 
@@ -180,7 +197,7 @@ void readMessage() {
 }
 
 /**
- * Sets all values to 0
+ * Reset the incomming message variable to 0
  */
 void resetMessage() {
   IN_Raw_Value = 00000000;
@@ -268,14 +285,14 @@ void drive(int ml4, int ml5, int mr6, int mr7, int speedL, int speedR) {
 }
 
 /**
- * Stops the motors
+ * Set motors in 'off' position
  */
 void stopDriving() {
   drive(LOW, LOW, LOW, LOW, 0, 0);
 }
 
 /**
- * Log a message when debugging is enabled
+ * Show messages at Serialmonitor when debugging is enabled
  */
 void logMessage(String message) {
   if (debug) {
@@ -283,4 +300,35 @@ void logMessage(String message) {
   }
 }
 
+void writeOUT_Values(int Value1, int Value2, int Value3){
+  OUT_Value1 = Value1;
+  OUT_Value2 = Value2;
+  OUT_Value3 = Value3;
+}
 
+void handlePing(){ 
+  OUT_ReceiverID = 8;
+  OUT_MessageID = 0;
+  writeOUT_Values(0,0,0);
+  OUT_Checksum = calculateChecksum(OUT_ReceiverID, OUT_SenderID, OUT_MessageID, OUT_Value1, OUT_Value2, OUT_Value3);
+  logMessage("Sending Ping"); 
+  sendMessage();
+}
+
+void handleArrived(){ 
+  OUT_ReceiverID = 8;
+  OUT_MessageID = 3;
+  writeOUT_Values(2,1,0);
+  OUT_Checksum = calculateChecksum(OUT_ReceiverID, OUT_SenderID, OUT_MessageID, OUT_Value1, OUT_Value2, OUT_Value3);
+  logMessage("Sending Arrived"); 
+  sendMessage();
+}
+
+void ErrorStop(){ 
+  OUT_ReceiverID = 8;
+  OUT_MessageID = 9;
+  writeOUT_Values(5,7,8);
+  OUT_Checksum = calculateChecksum(OUT_ReceiverID, OUT_SenderID, OUT_MessageID, OUT_Value1, OUT_Value2, OUT_Value3);
+  logMessage("Sending Error"); 
+  sendMessage();
+}
